26d758e5275c7fc0b2806c45772f9304fdb93c2a
haridtvt <tranvanhaidtvt@gmail.com>
2026-03-26 16:10:55 +0000

    Update Debug

--- web-portal/src/App.jsx
+++ web-portal/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function App() {
   const [status, setStatus] = useState("Idle");
-  const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+  const API_URL = import.meta.env.VITE_API_URL || "http://54.169.79.239:13000";
   const handleAttack = async () => {
     setStatus("Sending...");
     try {
